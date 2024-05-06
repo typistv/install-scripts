ff2a90062d3552a111d209c5fd53337ac8623053
typistv <typistchain@gmail.com>
2024-05-06 08:21:32 +0000

    change install_ssh_init_passwd.sh

--- scripts/install_ssh_init_passwd.sh
+++ scripts/install_ssh_init_passwd.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-# 安装 OpenSSH 服务器
-yum install -y openssh-server
-
-# 启动 OpenSSH 服务
-systemctl start sshd
-
-# 设置 OpenSSH 开机自启动
-systemctl enable sshd
-
-# 修改 root 用户密码为 123456
-echo "root:123456" | chpasswd
-
-# 显示修改后的密码
-echo "Root password changed to 123456"
+yum install -y openssh-server && systemctl start sshd && systemctl enable sshd && echo "root:123456" | chpasswd
