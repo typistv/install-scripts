2f8fb402b174dad1dad56a49b6215c9a3761a2e1
typistv <typistchain@gmail.com>
2024-05-10 06:28:02 +0000

    更新安装mysql的脚本

--- scripts/intsall_mysql8.sh
+++ scripts/intsall_mysql8.sh
@@ -10,28 +10,30 @@ backup_url="http://8.141.4.67/base/mysql/$file_name"
 # 下载超时时间（秒）
 timeout=10
 
-#过渡密码
+# 过渡密码
 interim_password="Aew&de!23"
 
-echo "Attempting to download from main URL..."
-if curl --output /dev/null --silent --head --fail --max-time $timeout "$main_url"; then
-    echo "Downloading from main URL..."
-    curl -O --max-time $timeout "$main_url"
-else
-    echo "Main URL timed out, switching to backup URL..."
-fi
+# 新密码
+new_password="123456"
 
-# 尝试下载备用链接
-if [ ! -f "$file_name" ]; then
-    echo "Attempting to download from backup URL..."
-    if curl --output /dev/null --silent --head --fail --max-time $timeout "$backup_url"; then
-        echo "Downloading from backup URL..."
-        curl -O --max-time $timeout "$backup_url"
+# 外部访问允许的 IP 地址，如果允许所有 IP 访问，可设置为 '0.0.0.0'
+bind_address="0.0.0.0"
+
+# 函数：从主要或备用链接下载文件
+download_file() {
+    local url="$1"
+    echo "Attempting to download from $url..."
+    if curl --output /dev/null --silent --head --fail --max-time $timeout "$url"; then
+        echo "Downloading from $url..."
+        curl -O --max-time $timeout "$url"
     else
-        echo "Backup URL timed out as well. Unable to download the file."
-        exit 1
+        echo "$url timed out. Unable to download the file."
+        return 1
     fi
-fi
+}
+
+# 下载文件
+download_file "$main_url" || download_file "$backup_url"
 
 # 检查文件是否存在，如果不存在则退出
 if [ ! -f "$file_name" ]; then
@@ -42,19 +44,8 @@ fi
 # 安装 MySQL Yum 存储库
 yum localinstall -y $file_name
 
-# 安装 yum-utils
-yum install -y yum-utils
-
-# 更新存储库缓存
-yum makecache
-
-# 禁用 mysql-8.4-lts-community 和 mysql-tools-8.4-lts-community 存储库
-yum-config-manager --disable mysql-8.4-lts-community
-yum-config-manager --disable mysql-tools-8.4-lts-community
-
 # 启用 mysql80-community 和 mysql-tools-community 存储库
-yum-config-manager --enable mysql80-community
-yum-config-manager --enable mysql-tools-community
+yum-config-manager --enable mysql80-community mysql-tools-community
 
 # 安装 MySQL 服务器
 yum install -y mysql-community-server
@@ -62,26 +53,44 @@ yum install -y mysql-community-server
 # 启动 MySQL 服务
 systemctl start mysqld
 
-# 等待一段时间，确保 MySQL 服务已启动
+# 等待 MySQL 服务启动
 sleep 10
 
 # 输出临时密码
 temp_password=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 # 修改密码为过渡密码
-mysql -u root -p"$temp_password" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY $interim_password;"
+mysql -u root -p"$temp_password" --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$interim_password';"
 
 # 修改 MySQL 密码验证等级为 LOW
-mysql -u root -p"$interim_password" -e "SET GLOBAL validate_password.policy=LOW;"
+mysql -u root -p"$interim_password" --connect-expired-password -e "SET GLOBAL validate_password.policy=LOW;"
 
-#修改 MySQL 密码验证长度为6
-mysql -u root -p"$interim_password" -e "SET GLOBAL validate_password.length=6;"
+# 修改 MySQL 密码验证长度为6
+mysql -u root -p"$interim_password" --connect-expired-password -e "SET GLOBAL validate_password.length=6;"
 
-# 修改 MySQL 密码为 '123456'
-mysql -u root -p"$interim_password" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123456';"
+# 修改 MySQL 密码为新密码
+mysql -u root -p"$interim_password" --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$new_password';"
 
-# 输出修改后的密码
-echo "MySQL password has been changed to '123456'."
+# 修改 MySQL 配置文件，允许外部访问
+sed -i "s/^bind-address\s*=.*$/bind-address = ${bind_address}/" /etc/my.cnf
+
+# 重启 MySQL 服务器使配置生效
+systemctl restart mysql
+
+# 连接 MySQL 并执行 SQL 命令
+mysql -u root -p"$new_password" <<EOF
+USE mysql;
+UPDATE user SET host='%' WHERE user='root';
+FLUSH PRIVILEGES;
+EOF
+
+# 授予root用户访问所有数据库的权限
+mysql -u root -p"$new_password" -e "GRANT ALL ON *.* TO 'root'@'%';"
+
+# 刷新权限
+mysql -u root -p"$new_password" -e "FLUSH PRIVILEGES;"
+
+echo "MySQL 已配置允许外部访问，并授予 root 用户访问所有数据库的权限。"
 
 # 退出脚本
 exit 0
